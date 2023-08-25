827793d97bbf9375ee889873f52b31134e92b8f1
mirae0831@o365.hufs.ac.kr <mirae0831@o365.hufs.ac.kr>
2023-08-25 18:02:48 +0900

    feat: 베팅페이지

--- src/pages/Game/Bet.js
+++ src/pages/Game/Bet.js
@@ -1,12 +1,26 @@
 import React from "react";
 import styled from 'styled-components';
+import SideBar from "../../components/SideBar/SideBar";
+import SideBarContents from "../../components/SideBar/SideBarContents";
+
+const Betting = styled.div`
+    width: 100%;
+    min-height: 100vh;
+    background-color: #1D1D1D;
+    color: #FFFFFF;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: flex-start;
+`
 
 const Bet = ()=> {
     return (
-      <div>
-        Betting
-      </div>
+      <Betting>
+        <SideBar><SideBarContents/></SideBar>
+      </Betting>
     );
   }
   
   export default Bet;
+
